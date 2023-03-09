471e8f1840169ba188f1c0b54b7a0444680722ab
jaehwan <terry0902@naver.com>
2023-03-09 19:43:44 +0900

    initial version of main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+def twoSum(nums:list[int],target:int)->list[int]:
+    for i in range(0,len(nums)-1):
+        if nums[i]+nums[i+1]==target:
+            return [i,i+1]
+
+    return [len(nums)-1,len(nums)]
