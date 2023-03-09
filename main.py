1d7f19b500bf0bf60ad215edde7642ea15d4176d
jaehwan <terry0902@naver.com>
2023-03-09 19:57:38 +0900

    final

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def twoSum(self,nums:list[int],target:int)->list[int]:
+def twoSum(nums:list[int],target:int)->list[int]:
     for i in range(0,len(nums)-1):
         if nums[i]+nums[i+1]==target:
             return [i,i+1]
